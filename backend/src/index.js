8289256d00d0a810bd947d72806aa7e2f809c48f
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-27 22:10:51 +0800

    fix: cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://timchat.vercel.app",
   credentials: true,
 }));
 
