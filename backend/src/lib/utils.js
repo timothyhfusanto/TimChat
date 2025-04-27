660c788d13c63fccf2b51770058406993721b130
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-27 22:52:21 +0800

    fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,15 +2,15 @@ import jwt from "jsonwebtoken"
 
 export const generateToken = (userId, res) => {
 
-	const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "7d"
 	});
 
 	res.cookie("jwt", token, {
-		httpOnly: true, // prevent XSS attacks (cross-site scripting attack) by disallowing the client to access the cookie)
-		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-		sameSite: "Strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development"
+		httpOnly: true,
+		secure: process.env.NODE_ENV !== "development", // true in production
+		sameSite: process.env.NODE_ENV !== "development" ? "None" : "Lax", // ✅ dynamic: None for production, Lax for dev
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 
 	return token;
