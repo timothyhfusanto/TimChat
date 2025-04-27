4c88b10cdd95a2abc8f63f2be8b668f10e461ab5
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-28 00:25:29 +0800

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,13 +6,13 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
-
 import path from "path";
 
 dotenv.config();
+connectDB();
 
-const PORT = process.env.PORT
 const __dirname = path.resolve();
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({
   origin: ["https://timchat.vercel.app", "http://localhost:5173"],
@@ -26,17 +26,18 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+// 4. API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT);
-  connectDB();
+  console.log("Server running on PORT:", PORT);
 });
