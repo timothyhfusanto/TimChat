e90c790e70afbe1cee791f80afd8dc71e8503645
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-27 22:22:16 +0800

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,14 +17,18 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://timchat.vercel.app",
+  origin: "https://timchat.vercel.app/*",
   credentials: true,
 }));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if (process.env.NODE_ENV === "production" ) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
