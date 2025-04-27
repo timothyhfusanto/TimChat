a9187305c29c5c7bd7b6af400d1859ad9c0054d9
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-28 00:40:43 +0800

    fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
 		httpOnly: true,
 		secure: true,
 		sameSite: "None",
-		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 
 	return token;
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,7 +4,6 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt
-    console.log("Token from cookies:", token);
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided" });
     }
